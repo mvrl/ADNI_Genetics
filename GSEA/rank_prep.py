23bc7d01cddc11452a4cdd1cde8b9c9b65c06220
Subash33 <44344741+Subash33@users.noreply.github.com>
2021-04-23 22:34:16 -0400

    GSEA files

--- GSEA/rank_prep.py
+++ GSEA/rank_prep.py
@@ -2,23 +2,39 @@
 import os
 import pandas as pd
 
-group='CN_Dementia'
+group='CN_AD'
 features = 2000
-rank_file = 't_test_geneExpr_Probefiltered.csv'
+#rank_file = 't_test_geneExpr_Probefiltered.csv'
+rank_file = 't_test_0.05_geneExpr_Unfiltered_bl.csv'
+#rank_file = 'GradientBoosting_1000_CN_AD_Unfiltered_50_extra_DX_bl.csv'
+#rank_file = 'GradientBoosting_700_CN_AD_Unfiltered_500_no_extra_DX_bl.csv'
 df = pd.read_csv(rank_file)
-ttest = df.sort_values(group).sort_values(group+'_c').iloc[0:features] #suffix _c to use the FDR corrected p values
+if 't_test' in rank_file:
+    rank = df.sort_values(group).sort_values(group+'_c').iloc[0:features] #suffix _c to use the FDR corrected p values
+else:
+    rank = df.sort_values('importance')
 
-print(ttest.head())
-print(ttest.iloc[0])
-with open('genes_ranked.rnk', 'w') as outfile:
-    text= "#Gene\tpvalue\n"
-    for i in range(len(ttest)):
-        gene = ttest.iloc[i]['Gene']
-        if "||" in gene:
-            gene = gene.split(' ')[0].strip()
-        pvalue = ttest.iloc[i][group]
+with open(rank_file.split('.')[0]+'.rnk', 'w') as outfile:
+    text= "#Gene\tscore\n"
+    for i in range(len(rank)):
+        if 't_test' in rank_file:
+            gene = rank.iloc[i]['Gene']
+            if gene in text or gene == 'nan':
+                continue
+            if "||" in gene:
+                gene = gene.split(' ')[0].strip()
+            score = rank.iloc[i][group]
+        else:
+            probe_gene = rank.iloc[i]['Gene'].split('_at_')
+            if len(probe_gene) == 2:
+                gene = probe_gene[1]
+            if gene in text or gene == 'nan':
+                continue
+            if "||" in gene:
+                gene = gene.split(' ')[0].strip()
+            score = rank.iloc[i]['importance']
 
-        text = text + gene+ "\t" + str(pvalue) + "\n"
+        text = text + gene+ "\t" + str(score) + "\n"
 
     text = text.strip()
 
