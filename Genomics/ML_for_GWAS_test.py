b7b6836089fb5ed580d6625753220932ef8a2880
Subash Khanal <subash.khanal33@gmail.com>
2021-06-10 12:22:19 -0400

    Final commits

--- Genomics/ML_for_GWAS_test.py
+++ Genomics/ML_for_GWAS_test.py
@@ -1,94 +1,46 @@
-## This is just .py file for the jupyter notebook. So it is not well organized
-
+from utilities import prepare_targets, data_prep, sequence_parser, GWAS_data_prep, GridSearch, save_results
 import os
 import pandas as pd
-from collections import Counter
-from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.feature_selection import RFECV
-from sklearn.ensemble import (RandomForestClassifier,
-                              GradientBoostingClassifier)
-from sklearn.model_selection import RandomizedSearchCV
-from imblearn.pipeline import Pipeline
-from imblearn.over_sampling import ADASYN, SMOTE, SMOTENC
+from sklearn.ensemble import GradientBoostingClassifier                             
+from imblearn.over_sampling import SMOTENC
+from collections import Counter
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import make_scorer
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_curve, auc, f1_score, roc_auc_score, balanced_accuracy_score
+from sklearn.metrics import roc_curve, roc_auc_score, balanced_accuracy_score
 from easydict import EasyDict as edict
 import itertools
 import warnings
 warnings.filterwarnings("ignore")
-################################################################################################
-#                                           UTILITIES
-################################################################################################
+
+#########################################################################
 SEED = 1
 np.random.seed(SEED)
-
-def prepare_targets(y,groups):
-    class1 = groups.split('_')[0]
-    class2 = groups.split('_')[1]
-    count_dict = Counter(y)
-    class1_count = count_dict[class1]
-    class2_count = count_dict[class2]
-    ## Label minority class = 1 and majority class = 0
-    if  class1_count > class2_count:
-        count_dict[class1] = int(0)
-        count_dict[class2] = int(1)
-    else:
-        count_dict[class1] = int(1)
-        count_dict[class2] = int(0)
-    op = [count_dict[i] for i in y]
-    return np.asarray(op)
-
-#Very inefficient approach! but is easier to visualize in my head
-def data_prep(df,groups): #This takes the dataframe and returns the one hot encoded expansion of input features
-    target = prepare_targets(list(df.DIAG),groups)
-    df1 = df.drop(columns=['PTID','DIAG']).reset_index(drop=True) #Patient ID and DIAG not needed
-    num_cols = ['AGE','EDU']
-    cat_cols = list(set(df1.columns) - set(num_cols)) #Categorical features
-    expand_cat = ['AGE','EDU'] #List of expanded categorical columns
-    for cat in cat_cols:
-        expand_cat = expand_cat + [str(cat)+'_'+ c for c in list(set(df1[cat]))]
-    df_out = pd.DataFrame(columns=list(expand_cat))
-    df_out['AGE'] = df1.AGE
-    df_out['EDU'] = df1.EDU
-    for i in range(len(df1)):
-        row = df1.iloc[i]
-        for col in cat_cols:
-            item = row[col]
-            df_out.at[i,str(col)+'_'+ item] = str(1)
-        
-    df_out = df_out.fillna(str(0))
-    return df_out, target.ravel()
-
+RESULTS = 'results' # Name of results folder, 'results' for overall grid search, results_test1, results_test2, results_test3 for 3 rounds of best model and features.
+data_path = '/home/skh259/LinLab/LinLab/ADNI_Genetics/Genomics/'
+results_path = os.path.join(data_path,RESULTS)
 ##########################################################################################################
-def train_ADNI(groups='CN_AD',features=1000):
+def train_ADNI(groups='CN_AD',features=1000,n_estimators=1050):
     
     groups = groups
     print("EXPERIMENT LOG FOR:",groups)
     print('\n')
-    data_path = '/home/skh259/LinLab/LinLab/ADNI_Genetics/Genomics/'
+    
     #Number of top SNPs to take as features
     N = features
-    
-    df_final = pd.read_csv(os.path.join(data_path,'GWAS12_data_Dx_bl.csv'),na_values=["00"])
-    df_final = df_final.iloc[:, 0:N+6] #Only top N snps
-    df_final = df_final.drop(columns=['Unnamed: 0'])
-    df_final.dropna(inplace=True)
-    print('Label distribution on GWAS generated file after dropping Missing individuals')
-    print(Counter(df_final.DIAG))
-    df, y = data_prep(df_final,groups)
+    df, y = GWAS_data_prep(groups,data_path,features)
     print("Shape of final data BEFORE FEATURE SELECTION")
     print(df.shape, y.shape)
-    
-    rank_df = pd.read_csv(os.path.join(data_path,'CN_AD_Classification_ranked_149_1000_features.csv'))
+    fname = '_'.join([groups,str(features),str(n_estimators)])
+    rank_df = pd.read_csv(os.path.join(data_path,'results','Features_ranked_for_CN_AD_1000_prune.csv'))
     selectors = list(rank_df['features'])
     df = df.loc[:, selectors]
     print("Shape of final data AFTER FEATURE SELECTION")
     print(df.shape, y.shape)
+    print("Label distribution ater final feature selection")
+    label_dist = Counter(y)
+    print(label_dist)
     final_N = df.shape[1]
     cat_columns = list(set(df.columns) - set(['AGE','EDU']))
     cat_columns_index = [i for i in range(len(df.columns)) if df.columns[i] in cat_columns]
@@ -109,7 +61,7 @@ def train_ADNI(groups='CN_AD',features=1000):
         
         X_test = X.iloc[test]
         y_test = y[test]
-        n_estimators = 1000
+        n_estimators = n_estimators
         model = GradientBoostingClassifier(random_state=SEED,n_estimators=n_estimators)
         oversample = SMOTENC(sampling_strategy=0.7, k_neighbors=7, categorical_features = cat_columns_index,random_state=SEED)
         X_train, y_train = oversample.fit_resample(X_train, y_train)
@@ -124,47 +76,27 @@ def train_ADNI(groups='CN_AD',features=1000):
         aucs.append(roc_auc)
         imp.append(model.feature_importances_)
 
-    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-            label='Chance', alpha=.8)
-
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(mean_fpr, mean_tpr, color='b',
-            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-            lw=2, alpha=.8)
-
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                    label=r'$\pm$ 1 std. dev.')
-
-    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-        title="Receiver operating characteristic")
-    ax.legend(loc="lower right")
-    plt.show()
-    plt.savefig(os.path.join(data_path,'ROC_for:'+groups+'_'+str(features)+'.png'))
-    print('for total of ',final_N,"Features")
-    print('Mean Balanced Accuracy:',sum(acc)/len(acc))
-    print('Mean AUC:',sum(aucs)/len(aucs))
-
-    imp = np.array(imp)
-    imp = imp.mean(axis=0)
-
-    imp_df = pd.DataFrame(columns=['features','importance'])
-    imp_df['features'] = list(X.columns)
-    imp_df['importance'] = imp
-
-    imp_df_sorted = imp_df.sort_values(by=['importance'],ascending=False)
-    imp_df_sorted.to_csv(os.path.join(data_path,groups+'_Classification_ranked_'+str(features)+'_features.csv'))
-
+    final_ACC = sum(acc)/len(acc)
+    final_AUC = sum(aucs)/len(aucs)
+    save_results(X,ax,imp,tprs, mean_fpr,aucs,acc,results_path,final_N,fname)
     print("END OF THE EXPERIMENT\n")
 
     plt.close('all')
-    return sum(acc)/len(acc), sum(aucs)/len(aucs)
+    return final_ACC, final_AUC, final_N, label_dist, n_estimators
 
-acc, my_auc = train_ADNI(features=1000,
-                groups = 'CN_AD'       
-               )
+
+if  __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--features', type=int, help='Number of features to be used', default=1000)
+    parser.add_argument('--n_estimators', type=int, help='Number of estimators for best model', default=1050)
+    parser.add_argument('--groups', type=str, help='binary classes to be classified ', default='CN_AD')
+    args = parser.parse_args()
+    
+    acc, my_auc, final_N, label_dist,n_estimators = train_ADNI(features=args.features,
+            n_estimators=args.n_estimators,
+            groups = args.groups       
+            )
+
+    print(acc,my_auc)
